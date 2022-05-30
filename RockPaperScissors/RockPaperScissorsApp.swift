266d999e2e4f03b837e17b379683e3964410d26a
Bern <bernnystrom@gmail.com>
2022-05-31 00:24:10 +0800

    🎉 (new): Initial commit
    Initial commit of prokect template with gitignore file.

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Bern N on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
