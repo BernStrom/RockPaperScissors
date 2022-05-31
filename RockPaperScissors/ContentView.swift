f6305445d65e3df8312e26ce1627c2727c2c0332
Bern <bernnystrom@gmail.com>
2022-05-31 17:13:07 +0800

    ✨ (feat): Add numerous base features of app
    Added @State properties and Views for displaying user score, number of rounds left, and a randomized winning or losing condition for the current game round with a basic layout.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gameMoves = ["👊", "👋", "✌️"]
+    @State private var appChoice = Int.random(in: 0...2)
+    @State private var playerShouldWin = Bool.random()
+    @State private var playerScore = 0
+    @State private var numQuestions = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            
+            VStack {
+                VStack(spacing: 15) {
+                    Text("Select your move based on the conditions below")
+                        .font(.subheadline.weight(.heavy))
+                        .foregroundStyle(.secondary)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        VStack {
+                            Text("Game Move:")
+                                .font(.title3.weight(.light))
+                            
+                            Text(gameMoves[appChoice])
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Text("Win or Lose Round:")
+                                .font(.title3.weight(.light))
+                            
+                            if playerShouldWin {
+                                Text("Win")
+                                    .font(.largeTitle.weight(.semibold))
+                            } else {
+                                Text("Lose")
+                                    .font(.largeTitle.weight(.semibold))
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+            
+            Spacer()
+            Spacer()
+            
+            Text("Question \(numQuestions) of 10")
+                .font(.headline)
+            
+            Spacer()
+            
+            Text("Score: \(playerScore)")
+                .font(.title.bold())
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
